f31eba86831bed8be5abe9ea68e894e3977c1967
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-06-16 02:15:26 +0900

    PrimeVue導入

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,16 @@ import App from "./App.vue";
 
 const app = createApp(App);
 
+// PrimeVue
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import PrimeButton from "primevue/button";
+import InputText from "primevue/inputtext";
+
+app.use(PrimeVue);
+app.component("PrimeButton", PrimeButton);
+app.component("InputText", InputText);
+
 app.mount("#app");
