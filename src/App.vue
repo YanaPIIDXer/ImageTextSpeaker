05205e827da410fcf0c3b8ef081940bc92414d7a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-06-16 02:48:48 +0900

    画像をBase64に変換して読み込む処理を実装

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts">
+import FileUpload from "primevue/fileupload";
+import { ref } from "vue";
+
+const loadedImage = ref("");
+
+/**
+ * アップロードした
+ */
+const onUpload = (e: any) => {
+  const file = e.files[0];
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    const base64 = reader.result as string;
+    loadedImage.value = base64;
+  }
+
+  reader.readAsDataURL(file);
+}
 </script>
 
 <template>
+  <FileUpload
+    accept="image/*"
+    :auto="true"
+    :custom-upload="true"
+    @uploader="onUpload"
+  />
+  <img v-if="loadedImage" :src="loadedImage" />
 </template>
 
 <style lang="sass" scoped>
